6a256e446cd32ebadbbf204875e77c1179e4a7b0
iketown <be@ike.works>
2022-01-24 16:16:24 -0600

    no auth refs 4:16

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,9 @@ import * as React from "react";
 import type { AppProps } from "next/app";
 import { CacheProvider, EmotionCache } from "@emotion/react";
 import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
-import { AuthCtxProvider } from "~/contexts/AuthCtx";
-import { UserCtxProvider } from "~/contexts/UserCtx";
-import { PostCtxProvider } from "~/contexts/PostCtx";
+// import { AuthCtxProvider } from "~/contexts/AuthCtx";
+// import { UserCtxProvider } from "~/contexts/UserCtx";
+// import { PostCtxProvider } from "~/contexts/PostCtx";
 import { SnackbarProvider } from "notistack";
 
 import "@fontsource/roboto/300.css";
@@ -33,18 +33,18 @@ const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
     <CacheProvider value={emotionCache}>
       {/* <AuthCtxProvider> */}
       {/* <UserCtxProvider> */}
-      <TagCtxProvider>
-        <PostCtxProvider>
-          <SnackbarProvider>
-            <ThemeProvider theme={lightTheme}>
-              <CssBaseline />
-              <Layout>
-                <Component {...pageProps} />
-              </Layout>
-            </ThemeProvider>
-          </SnackbarProvider>
-        </PostCtxProvider>
-      </TagCtxProvider>
+      {/* <TagCtxProvider> */}
+      {/* <PostCtxProvider> */}
+      <SnackbarProvider>
+        <ThemeProvider theme={lightTheme}>
+          <CssBaseline />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ThemeProvider>
+      </SnackbarProvider>
+      {/* </PostCtxProvider> */}
+      {/* </TagCtxProvider> */}
       {/* </UserCtxProvider> */}
       {/* </AuthCtxProvider> */}
     </CacheProvider>
