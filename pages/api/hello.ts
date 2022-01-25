b94fe3c89c02ad630099a4d284690b89ecc5e563
iketown <be@ike.works>
2022-01-25 04:49:18 -0600

    api test with user 4:49

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,19 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import nookies from "nookies";
+import { adminAuth } from "~/utils/firebase.server";
 
 type Data = {
   name: string;
   credential: any;
   cooks: any;
+  decodedToken: any;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   const GAC = process.env.GOOGLE_APPLICATION_CREDENTIALS!;
   const credential = JSON.parse(Buffer.from(GAC, "base64").toString());
   const cooks = nookies.get({ req });
-  res.status(200).json({ name: "John Doe", credential, cooks });
+  let decodedToken;
+  if (cooks.token) {
+    decodedToken = await adminAuth.verifyIdToken(cooks.token);
+  }
+  res.status(200).json({ name: "John Doe", credential, cooks, decodedToken });
 }
